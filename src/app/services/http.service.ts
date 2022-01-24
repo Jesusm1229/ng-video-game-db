b15dac4bd34104c0f057771f68da9b31ca38ebff
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2022-01-24 15:03:38 -0400

    Styling detail component

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { forkJoin, map, Observable } from 'rxjs';
 import { APIResponse, Game } from '../models';
 import { environment as env } from 'src/environments/environment';
 
@@ -26,5 +26,28 @@ export class HttpService {
     });
   }
 
+  getGameDetails(id: string): Observable<Game> {
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
+    const gameTrailersRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/movies`
+    );
+    const gameScreenshotsRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/screenshots`
+    );
+
+    return forkJoin({
+      gameInfoRequest,
+      gameScreenshotsRequest,
+      gameTrailersRequest,
+    }).pipe(
+      map((resp: any) => {
+        return {
+          ...resp['gameInfoRequest'],
+          screenshots: resp['gameScreenshotsRequest']?.results,
+          trailers: resp['gameTrailersRequest']?.results,
+        };
+      })
+    );
+  }
   
 }
